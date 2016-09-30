078e1879e1169b3e257a800ecb712f5a756adf5b
musquash <musquash@gmx.de>
2016-09-30 13:17:56 +0200

    Initial Upload for lectures

---
+++ Class definition.py
@@ -0,0 +1,20 @@
+# Class definition
+class Car(object):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    def __init__(self, name, factory, number_wheels):
+        self.name = name
+        self.factory = factory
+        self.number_wheels = number_wheels
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+Opel = Car("Corsa", "Opel", 4)
+giraffe = Animal("Bruce", 1, False)
+panda = Car("Skoda", "Yeti", 5)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
